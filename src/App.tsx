1ffc90fa6d469d51441298a862c708bdbad71aa6
wminaung <minwin243@gmail.com>
2023-01-09 04:13:06 +0630

    Final commited

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import "./App.css";
+import { Text } from "./components/polymorphic/Text";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Text as="h1" size="md">
+        Heading
+      </Text>
+      <Text as="p" size="md">
+        Heading
+      </Text>
+      <Text as="label" htmlFor="inputId" size="sm" color="secondary">
+        Heading
+      </Text>
+    </div>
+  );
 }
 
 export default App;
